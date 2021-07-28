7b08aea818995a8571e703809e169c771a5d5a7b
QarsoonMeel <qarsoonmeel@gmail.com>
2021-07-28 11:10:35 +0100

    Simplified the patch a bit

--- Source/AbilitiesCooldown/AbilitiesCooldown/NotificationPatch.cs
+++ Source/AbilitiesCooldown/AbilitiesCooldown/NotificationPatch.cs
@@ -13,10 +13,8 @@ namespace AbilitiesCooldown
         [HarmonyPostfix]
         public static void NotificationPostfix(Ability __instance)
         {
-            if (__instance.def.defName == "Convert" && (int)cooldownTicks.GetValue(__instance) == 0)
+            if (__instance.def.defName == "Convert" && (int)cooldownTicks.GetValue(__instance) == 1)
             {
-                int cooldown = -1;
-                cooldownTicks.SetValue(__instance, cooldown);
                 Messages.Message("Your chaplain can cast abilities again.", __instance.pawn, MessageTypeDefOf.PositiveEvent, false);
             }
         }
